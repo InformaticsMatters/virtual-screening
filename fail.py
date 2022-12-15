483bfac19a20215eca107c3c3def58abcbc2a22c
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2022-12-15 12:14:07 +0000

    fail.py - a job that allows failures to be tested

---
+++ fail.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Copyright 2022 Informatics Matters Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Job that allows failures to be tested
+"""
+
+import argparse
+from dm_job_utilities.dm_log import DmLog
+
+def main():
+    # Example usage:
+    # ./fail.py --mode error|empty|missing
+
+    parser = argparse.ArgumentParser(description='Fail')
+    parser.add_argument('-m', '--mode', required=True, choices=['error', 'empty', 'missing'], help='Fail mode')
+
+    args = parser.parse_args()
+    DmLog.emit_event("fail: ", args)
+
+    if args.mode == 'error':
+        DmLog.emit_event('Job failed')
+        exit(1)
+    elif args.mode == 'empty':
+        with open('test-fail-results.txt', 'wt') as out:
+            DmLog.emit_event('Empty results.txt')
+    else:
+        DmLog.emit_event('Missing results.txt')
+
+
+if __name__ == "__main__":
+    main()
